a4d5ec0350ab6d17645894c09f9fdec5a514baf2
unknown <syabab45@gmail.com>
2015-04-10 08:38:09 +0700

    first update

---
+++ application/views/navbar.php
@@ -0,0 +1,50 @@
+<ul class="sidebar-menu">
+              <li>
+                  <a class="" href="<?php echo base_url();?>">
+                      <i class="icon-dashboard"></i>
+                      <span>Dashboard</span>
+                  </a>
+              </li>
+			  <li>
+                  <a class="" href="<?php echo base_url();?>dashboard/web_profile">
+                    <i class="icon-globe"></i>
+                    <span>Web Profile</span>
+                  </a>
+              </li>
+			  <li>
+                  <a class="" href="<?php echo base_url();?>dashboard/slider">
+                    <i class="icon-picture"></i>
+                    <span>Image Slider</span>
+                  </a>
+              </li>
+			  <li class="sub-menu">
+                  <a href="javascript:;" class="">
+                      <i class="icon-edit"></i>
+                      <span>Site Content</span>
+                      <span class="arrow"></span>
+                  </a>
+                  <ul class="sub">
+                      <li><a class="" href="#">Home</a></li>
+                      <li><a class="" href="#">Event</a></li>
+                      <li><a class="" href="#">Content</a></li>
+                      <li><a class="" href="#">Contact Us</a></li>
+                  </ul>
+              </li>
+			  <li class="sub-menu">
+                  <a href="javascript:;" class="">
+                      <i class="icon-cogs"></i>
+                      <span>Setting</span>
+                      <span class="arrow"></span>
+                  </a>
+                  <ul class="sub">
+                      <li><a class="" href="#">User Management</a></li>
+                      <li><a class="" href="<?php echo base_url();?>dashboard/my_account">My Account</a></li>
+                  </ul>
+              </li>
+			  <li>
+                  <a class="" href="<?php echo base_url();?>login/logout">
+                    <i class="icon-arrow-left"></i>
+                    <span>Log Out</span>
+                  </a>
+              </li>
+          </ul>
